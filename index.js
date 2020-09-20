1f57e3a570a21a2a699a458198715bf60f05f380
ikuka128 <70486142+ikuka128@users.noreply.github.com>
2020-09-21 04:16:00 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,65 +1,30 @@
-function loadScript(url) {
-    return new Promise(
-        (resolve) => {
-            const script = document.createElement('script');
-            script.src = url;
-            script.onload = resolve;
-            document.body.appendChild(script);
-        })
+function loadScript (url) {
+  return new Promise((resolve) => {
+    const script = document.createElement('script')
+    script.src = url
+    script.onload = resolve
+    document.body.appendChild(script)
+  })
 }
 
-function loadStyle(url){
-    return new Promise(
-        (resolve) => {
-            const link_tag = document.createElement('link');
-            link_tag.rel = 'stylesheet';
-            link_tag.type = 'text/css';
-            link_tag.href = url;
-            link_tag.onload = resolve;
-            document.head.appendChild(link_tag);
-        })
+async function loadAllScripts (urls) {
+  for (let url of urls) {
+    await loadScript(url)
+  }
 }
 
+$(function(){
+  const args = {}
+  document.location.search.substring(1).split('&').forEach((s) => {
+    let [name, value] = s.split('=')
+    args[name] = decodeURIComponent(value)
+  })
+  const codelist = args['code']
 
-async function loadAllScripts(urls) {
-    for(let url of urls){
-        await loadScript(url);
-    }
-}
+  console.log('codelist', codelist)
 
-async function loadAllStyles(urls){
-    for(let url of urls){
-        await loadStyle(url);
-    }
-}
-
-
-
-window.addEventListener('DOMContentLoaded', function(){
-    const args = {};
-    // ?ˆÈ~‚ÌŒ‹‰Ê‚ðŽæ“¾‚·‚é
-    document.location.search.substring(1).split('&').forEach(
-        (s) => {
-            let [name, value] = s.split('=');
-            // decode
-            args[name] = decodeURIComponent(value);
-        })
-    const codelist = args['code'];
-    const stylelist = args['style'];
-
-    console.log('codelist', codelist);
-    console.log('stylelist', stylelist);
-
-    if (codelist) {
-        let urls = codelist.split(/,/);
-        loadAllScripts(urls);
-    }
-
-    if(stylelist){
-        let urls = stylelist.split(/,/);
-        loadAllStyles(urls);
-    }
-
-}
-
-)
+  if (codelist) {
+    let urls = codelist.split(/,/)
+    loadAllScripts(urls)
+  }
+})
